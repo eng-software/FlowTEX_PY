135521794f32229d2167d7b75603dc606758494e
hc.coser <hc.coser@gmail.com>
2021-04-24 03:33:48 -0300

    Atualização do cabeçalho dos códigos fonte

--- TesteFlowTEX.py
+++ TesteFlowTEX.py
@@ -3,6 +3,19 @@
 
     Created: 23/04/2021 12:30:00
     Author: henrique.coser
+
+   This example code is in the Public Domain
+
+   This software is distributed on an "AS IS" BASIS, 
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+   either express or implied.
+
+   Este código de exemplo é de uso publico,
+
+   Este software é distribuido na condição "COMO ESTÁ",
+   e NÃO SÃO APLICÁVEIS QUAISQUER GARANTIAS, implicitas 
+   ou explicitas
+
 '''
 import serial, serial.tools.list_ports, time
 import TexNet
